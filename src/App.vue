181dc0267585bc99a85bae8173e7f3a84cd44e23
hridoymahmud71 <hridoymahmud71@gmail.com>
2019-12-04 18:10:29 +0600

    Wed, Dec  4, 2019  6:08:33 PM

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  },
+  data(){
+    return {
+      todos:[
+          {
+            id:1,
+            title:"Todo One",
+            completed:true
+          },
+          {
+            id:2,
+            title:"Todo Two",
+            completed:false
+          },
+          {
+            id:3,
+            title:"Todo Three",
+            completed:true
+          },
+      ],
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
